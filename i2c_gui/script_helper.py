e1f2a4bd03c82dd9a4e8fe96279be1640b837e85
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2023-03-09 17:39:32 +0000

    Added utility function to the script helper

--- i2c_gui/script_helper.py
+++ i2c_gui/script_helper.py
@@ -15,3 +15,37 @@ class ScriptHelper(GUI_Helper):
 
     def send_message(self, message:str):
         print("GUI Message: {}".format(message))
+    def get_all_indexed_blocks(self, indexer_info, block_name):
+        indexed_blocks = {}
+        for idx in range(len(indexer_info['vars'])):
+            var = indexer_info['vars'][idx]
+            min = indexer_info['min'][idx]
+            max = indexer_info['max'][idx]
+
+            old_indexed_blocks = indexed_blocks
+            indexed_blocks = {}
+
+            if var == "block" and min is None and max is None:
+                param = block_name
+                if len(old_indexed_blocks) == 0:
+                    indexed_blocks[param] = {
+                        'params': {'block': param},
+                    }
+                else:
+                    for old in old_indexed_blocks:
+                        indexed_blocks[old + ":" + param] = {}
+                        indexed_blocks[old + ":" + param]['params'] = old_indexed_blocks[old]['params']
+                        indexed_blocks[old + ":" + param]['params']['block'] = str(param)
+            else:
+                for val_idx in range(max - min):
+                    i = min + val_idx
+                    if len(old_indexed_blocks) == 0:
+                        indexed_blocks[i] = {
+                            'params': {var: i},
+                        }
+                    else:
+                        for old in old_indexed_blocks:
+                            indexed_blocks[old + ":" + str(i)] = {}
+                            indexed_blocks[old + ":" + str(i)]['params'] = (old_indexed_blocks[old]['params']).copy()
+                            indexed_blocks[old + ":" + str(i)]['params'][var] = i
+        return indexed_blocks
