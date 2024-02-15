e8a585f6b25ccf83d5379c12fe316afac787979d
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2024-02-15 16:43:06 -0600

    Fixed sizing for the others too

--- i2c_gui/register_block_array_interface.py
+++ i2c_gui/register_block_array_interface.py
@@ -155,7 +155,7 @@ class Register_Block_Array_Interface(Base_Interface):
 
         if first_register is not None:
             self._frame.update_idletasks()
-            self._register_orig_size = self._register_handle[first_register].get_size()
+            self._register_orig_size = self._register_handle[first_register].get_required_size()
             self._current_displayed_columns = register_columns
             element.bind("<Configure>", self._check_for_resize, add='+')
 
