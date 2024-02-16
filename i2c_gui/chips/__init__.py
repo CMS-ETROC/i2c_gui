70d34f6ae16257f6a705ee85b2eff3fa42a6d52b
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2024-02-16 17:39:35 -0600

    Added the class for the AD5593R tot he init module

--- i2c_gui/chips/__init__.py
+++ i2c_gui/chips/__init__.py
@@ -24,8 +24,10 @@ from __future__ import annotations
 
 from .etroc1_chip import ETROC1_Chip
 from .etroc2_chip import ETROC2_Chip
+from .ad5593R_chip import AD5593R_Chip
 
 __all__ = [
     "ETROC1_Chip",
     "ETROC2_Chip",
+    "AD5593R_Chip",
 ]
