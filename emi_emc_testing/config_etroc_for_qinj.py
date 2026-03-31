92f31843e6e8b9716fed9bc858e8c161db951818
Jongho Lee <jongapples@gmail.com>
2026-03-31 10:44:55 -0500

    test load basline history

--- emi_emc_testing/config_etroc_for_qinj.py
+++ emi_emc_testing/config_etroc_for_qinj.py
@@ -1,6 +1,7 @@
 import yaml
 import argparse
 import new_wrapper_i2cGui2
+import load_bl_history
 import plotter
 
 def load_config(config_path):
@@ -11,6 +12,7 @@ def config_for_qinj(
         config_path,
         history_note,
         do_checks: bool = False,
+        load_histroy: bool = False,
 ):
     # Load the external YAML data
     cfg = load_config(config_path)
@@ -36,20 +38,33 @@ def config_for_qinj(
     i2c_conn.batch_disable_all_chips(cfg['power_mode'])
     i2c_conn.batch_set_chip_peripherals()
 
-    ### Run calibration
-    pixels_of_interest = [(r, c) for r in range(16) for c in range(16)]
-    i2c_conn.batch_auto_calibrate_chip(pixels_of_interest)
+    ### Run OR Load Calibration
+    if load_histroy:
+        historical_dfs = load_bl_history.interactive_load_baselines(cfg['output_path'], names)
 
-    ### Save calibration results
-    for _, df in i2c_conn.BL_df.items():
-        plotter.save_baselines(df, hist_dir=cfg['output_path'], save_notes=history_note)
+        # Inject the loaded DataFrames directly into the wrapper's memory
+        for addr, chip_name in zip(chip_addrs, names):
+            print(historical_dfs[chip_name])
+            # if not historical_dfs[chip_name].empty:
+            #     i2c_conn.BL_df[addr] = historical_dfs[chip_name]
+            # else:
+            #     raise RuntimeError(f"Cannot proceed: Missing historical baseline data for {chip_name}")
+
+    else:
+        pass
+        # pixels_of_interest = [(r, c) for r in range(16) for c in range(16)]
+        # i2c_conn.batch_auto_calibrate_chip(pixels_of_interest)
+
+        # ### Save calibration results
+        # for _, df in i2c_conn.BL_df.items():
+        #     plotter.save_baselines(df, hist_dir=cfg['output_path'], save_notes=history_note)
 
     ### Config pixels for qinj
-    pixels_for_qinj = [(2, 2), (2, 10), (10, 2), (10, 10), (5, 5), (5, 13), (13, 5), (13, 13)]
-    i2c_conn.batch_enable_pixels(pixels_for_qinj, Qsel=cfg['qsel'], offset=cfg['offset'])
+    # pixels_for_qinj = [(2, 2), (2, 10), (10, 2), (10, 10), (5, 5), (5, 13), (13, 5), (13, 13)]
+    # i2c_conn.batch_enable_pixels(pixels_for_qinj, Qsel=cfg['qsel'], offset=cfg['offset'])
 
-    ### Print Invalid FC, good case: counter didn't change!
-    i2c_conn.batch_check_invalid_fc()
+    # ### Print Invalid FC, good case: counter didn't change!
+    # i2c_conn.batch_check_invalid_fc()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='ETROC2 YAML-based Config Tool')
@@ -77,5 +92,12 @@ if __name__ == "__main__":
         dest = 'doChecks',
     )
 
+    parser.add_argument(
+        '--load-history',
+        action='store_true',
+        help='Skip auto-calibration and choose a baseline from history',
+        dest='load_history'
+    )
+
     args = parser.parse_args()
-    config_for_qinj(args.config, args.note, do_checks=args.doChecks)
+    config_for_qinj(args.config, args.note, do_checks=args.doChecks, load_histroy=args.load_history)
